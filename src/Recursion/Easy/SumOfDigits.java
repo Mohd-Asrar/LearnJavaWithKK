a9845bcc9a7da21e8444c295ae708bd4e73b7cf4
Mohd Asrar <asrar1309@gmail.com>
2023-06-01 21:05:31 +0530

    Add One recursion Question

---
+++ src/Recursion/Easy/SumOfDigits.java
@@ -0,0 +1,17 @@
+package Recursion.Easy;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+
+        System.out.println(sumOfDigits(289105));
+
+    }
+
+    public static int sumOfDigits(int n){
+        if(n==0){
+            return 0;
+        }
+
+        return n%10 + ( sumOfDigits(n/10));
+    }
+}
